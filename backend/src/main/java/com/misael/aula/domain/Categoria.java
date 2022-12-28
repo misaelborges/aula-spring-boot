7ee6ed1fa4432b15b1444e40de0c1355f8434a61
Misael Borges <mizaelborges2011@gmail.com>
2022-12-28 15:50:52 -0300

    classes de dominio iniciadas

---
+++ backend/src/main/java/com/misael/aula/domain/Categoria.java
@@ -0,0 +1,80 @@
+package com.misael.aula.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Categoria {
+    private Long id;
+    private String nome;
+    private String descricao;
+
+    private List<Livro> livros = new ArrayList<>();
+
+    public Categoria() {
+        super();
+    }
+
+    public Categoria(Long id, String nome, String descricao) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if(obj == null)
+            return false;
+        if(getClass() != null)
+            return false;
+        Categoria other = (Categoria) obj;
+        if(id == null) {
+            if(other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
---
+++ backend/src/main/java/com/misael/aula/domain/Livro.java
@@ -0,0 +1,77 @@
+package com.misael.aula.domain;
+
+public class Livro {
+    private Long id;
+    private String titulo;
+    private String nome_autor;
+    private String texto;
+
+    public Livro() {
+        super();
+    }
+
+    public Livro(Long id, String titulo, String nome_autor, String texto) {
+        this.id =  id;
+        this.titulo = titulo;
+        this.nome_autor = nome_autor;
+        this.texto = texto;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNome_autor() {
+        return nome_autor;
+    }
+
+    public void setNome_autor(String nome_autor) {
+        this.nome_autor = nome_autor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if(obj == null)
+            return false;
+        if(getClass() != obj.getClass())
+            return false;
+        Livro other = (Livro) obj;
+        if(id == null) {
+            if(other.id != null)
+                return false;
+        } else if(id.equals(other.id))
+            return false;
+        return true;
+    }
+}
