4136ec91687d8e3ca82dc70acb4b6c27b43ec9b0
Misael Borges <mizaelborges2011@gmail.com>
2022-12-29 20:09:57 -0300

    atualização da classe Livro.java

--- backend/src/main/java/com/misael/aula/domain/Livro.java
+++ backend/src/main/java/com/misael/aula/domain/Livro.java
@@ -1,12 +1,15 @@
 package com.misael.aula.domain;
 
 import java.io.Serializable;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Livro implements Serializable {
 
     private static final long serialVersionUID = 1L;
